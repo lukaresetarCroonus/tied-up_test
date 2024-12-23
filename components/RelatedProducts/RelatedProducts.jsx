0d13a524c42d5a2710843ef7ae8df1f1d1618b12
luka.croonus <luka.resetar@croonus.com>
2024-12-23 15:42:08 +0100

    test query2

--- components/RelatedProducts/RelatedProducts.jsx
+++ components/RelatedProducts/RelatedProducts.jsx
@@ -3,11 +3,11 @@ import { Suspense, useState } from "react";
 import { Thumb } from "@/_components/shared";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper";
-import { useSuspenseQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { list } from "@/app/api/api";
 
 const RelatedProducts = ({ id }) => {
-  const { data: relatedProducts } = useSuspenseQuery({
+  const { data: relatedProducts } = useQuery({
     queryKey: ["relatedProducts"],
     queryFn: async () => {
       return await list(`/product-details/recommended/${id}`)?.then(
